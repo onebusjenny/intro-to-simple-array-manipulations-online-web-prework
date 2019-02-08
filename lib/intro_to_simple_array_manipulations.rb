efceac7fb648454cbe34472e9562c30c6263ff03
Jenny Chu <jchudesign0930@gmail.com>
2019-02-08 05:48:11 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -30,9 +30,11 @@ def shift_with_args (array)
   array.shift(2)
 end
 
-def using_concat (array)
+def using_concat (array,string)
+  array = ["raindrops on roses"]
+  string = ["whiskers on kittens","mario kart","flatiron school"]
   
-  array.concat(3) 
+  array.concat(string) 
 
   
 end
